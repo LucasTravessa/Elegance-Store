55a6eb1bfeeb2e64bf86ab7906954218cc844152
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-07-11 16:40:31 -0300

    identação de codigo

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -1,9 +1,9 @@
 import './footer.module.css';
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <div className="text-center p-1 bgTherd">
-            <p className='mt-3' style={{color: "white"}}>
+            <p className='mt-3' style={{ color: "white" }}>
                 © Copyright 2023 Elegance - All Rights Reserved
             </p>
         </div>
