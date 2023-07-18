8d65dcafefe0563247fc411d1848ae5ba2d54222
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-07-18 16:06:54 -0300

    footer aimation div

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -3,6 +3,8 @@ import styles from'./footer.module.css';
 import Card from "react-bootstrap/Card";
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
+
 
 export default function Footer() {
     const [status1, setStatus1] = useState(false);
@@ -26,30 +28,49 @@ export default function Footer() {
         setStatus4(!status4);
     }
 
+    const animationProps1 = useSpring({
+        height: status1 ? 'auto' : 0,
+        opacity: status1 ? 1 : 0,
+      });
+    
+      const animationProps2 = useSpring({
+        height: status2 ? 'auto' : 0,
+        opacity: status2 ? 1 : 0,
+      });
+    
+      const animationProps3 = useSpring({
+        height: status3 ? 'auto' : 0,
+        opacity: status3 ? 1 : 0,
+      });
+    
+      const animationProps4 = useSpring({
+        height: status4 ? 'auto' : 0,
+        opacity: status4 ? 1 : 0,
+      });
+    
+
     return (
             <div className="z-1 text-center p-1 bgTherd" id={styles.footer}>
                 <div className={styles.cards}>
 
-                    <Card className={styles.card}>
+                    <div className={styles.card}>
                         <h1 className={styles.title}>Elegance Rommanel</h1>
                         <p>Rod.Amaral Peixoto, 5223</p> 
                         <p>Centro </p> 
                         <p>Rio das Ostras - RJ, 28890-000</p>  
-                    </Card>
+                    </div>
 
-                    <Card className={styles.card}>
-                        <h1 className={styles.title}>Telefone:</h1>
-                        <p>(22)998313480</p>
-                        <h1 className={styles.title}>Instagram:</h1>
-                        <p>@elegance.rommanel</p>
-                    </Card>
+                    <div className={styles.card}>
+                        <p className='fs-5'>Telefone:<p className='fs-6'>(22)998313480</p></p>
+                        <p className=' fs-5'>Instagram:<p className='fs-6'>@elegance.rommanel</p></p>
+                    </div>
 
-                    <Card className={styles.card}>
+                    <div className={styles.card}>
                         <h1 className={styles.title}>Funcionamento:</h1>
                         <p>Segunda a sexta: 9:30 as 18:00</p>
                         <p>Sábado: 9:30 as 16:00</p>
                         <p>Domingo: Fechado</p>
-                    </Card>
+                    </div>
 
                 </div>               
                 
@@ -64,11 +85,13 @@ export default function Footer() {
                         </button>
 
                     </Card>
-                    <Card id={styles.cardbutton} className={`${status1 ? styles.footer_visible : styles.footer_hidden}`} style={{zIndex: '0'}}>
-                        <p >Rod.Amaral Peixoto, 5223</p> 
-                        <p >Centro </p> 
-                        <p >Rio das Ostras - RJ, 28890-000</p> 
-                    </Card>
+                    <animated.div style={animationProps1}>
+                        <Card id={styles.cardbutton} className={`${status1 ? styles.footer_visible : styles.footer_hidden}`} style={{zIndex: '0'}}>
+                            <p >Rod.Amaral Peixoto, 5223</p> 
+                            <p >Centro </p> 
+                            <p >Rio das Ostras - RJ, 28890-000</p> 
+                        </Card>
+                    </animated.div>
 
                     <Card className={styles.cardmob}>
 
@@ -78,9 +101,11 @@ export default function Footer() {
                         </button>
 
                     </Card>
-                    <Card id={styles.cardbutton} className={`${status2 ? styles.footer_visible : styles.footer_hidden}`} style={{zIndex: '0'}}>
-                        <p>(22)998313480</p>
-                    </Card>
+                    <animated.div style={animationProps2}>
+                        <Card id={styles.cardbutton} className={`${status2 ? styles.footer_visible : styles.footer_hidden}`} style={{zIndex: '0'}}>
+                            <p>(22)998313480</p>
+                        </Card>
+                    </animated.div>
 
                     <Card className={styles.cardmob}>
 
@@ -90,9 +115,11 @@ export default function Footer() {
                         </button>
 
                     </Card>
-                    <Card id={styles.cardbutton} className={`${status3 ? styles.footer_visible : styles.footer_hidden}`} style={{zIndex: '0'}}>
-                        <p>@elegance.rommanel</p>
-                    </Card>
+                    <animated.div style={animationProps3}>
+                        <Card id={styles.cardbutton} className={`${status3 ? styles.footer_visible : styles.footer_hidden}`} style={{zIndex: '0'}}>
+                            <p>@elegance.rommanel</p>
+                        </Card>
+                    </animated.div>
 
                     <Card className={styles.cardmob}>
 
@@ -102,11 +129,13 @@ export default function Footer() {
                         </button>
 
                     </Card>
+                    <animated.div style={animationProps4}>
                     <Card id={styles.cardbutton} className={`${status4 ? styles.footer_visible : styles.footer_hidden}`} style={{zIndex: '0'}}>
                         <p>Segunda a sexta: 9:30 as 18:00</p>
                         <p>Sábado: 9:30 as 16:00</p>
                         <p>Domingo: Fechado</p>
                     </Card>
+                    </animated.div>
                 </div>
 
                 <p className='mt-3' style={{ color: "white"}} id={styles.text}>
