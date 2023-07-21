9826c3e16c06d09f0589897fcff8fb7ad2adacd4
Lucas Travessa <lucas.cos.travessa@gmail.com>
2023-07-20 21:17:41 -0300

    Functional Map

--- src/app/information/information.tsx
+++ src/app/information/information.tsx
@@ -13,33 +13,6 @@ export default function Info() {
             <Map/>
         </Col>
       </Row>
-      <Row className={"bgSecondary m-5 border border-dark"} id={styles.contato}>
-        {/* <Row className='text-center'>
-          <Col>
-            <h1>Contato</h1>
-          </Col>
-        </Row> */}
-        <Row className='text-center'>
-          <Col>
-            <h4>Elegance Rommanel</h4>
-            <p>Rod. Amaral Peixoto, 5231</p>
-            <p>Centro</p>
-            <p>Rio das Ostras - RJ, 28890-000</p>
-          </Col>
-          <Col>
-            <h4>Telefone:</h4>
-            <p>(022)998313480</p>
-            <h4>Instagram:</h4>
-            <p>@elegance.rommanel</p>
-          </Col>
-          <Col>
-            <h4>Funcionamento:</h4>
-            <p>Segunda a sexta: 9:30 as 18:00</p>
-            <p>Sábado: 9:30 as 16:00</p>
-            <p>Domingo: Fechado</p>
-          </Col>
-        </Row>
-      </Row>
     </Container>
   );
 }
--- src/app/information/map.tsx
+++ src/app/information/map.tsx
@@ -1,12 +1,16 @@
-import { GoogleMap, Marker, useLoadScript, InfoWindow } from '@react-google-maps/api';
+import { GoogleMap, Marker, useLoadScript, InfoWindowF } from '@react-google-maps/api';
+import { useState } from 'react';
+import Link from 'next/link'
 
 const Map: React.FC = () => {
   const center = { lat: -22.523965275093655, lng: -41.939493840902806 }; // Coordenadas da localização da loja , -, 
-  const zoom = 15; // Nível de zoom do mapa
+  const zoom = 18; // Nível de zoom do mapa
 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, // Sua chave de API do Google Maps
   });
+
+  const [selected, setSelected] = useState(false);
 
   if (loadError) {
     return <div>Error loading map</div>;
@@ -22,10 +26,38 @@ const Map: React.FC = () => {
       center={center}
       zoom={zoom}
     >
-      <Marker position={center} title="Localização da loja" animation={google.maps.Animation.DROP}/> {/* Marcador na localização da loja */}
+      <Marker 
+      position={center} 
+      title="Localização da loja" 
+      animation={google.maps.Animation.DROP}
+      onClick={()=>{
+        setSelected(!selected);
+      }}
+      /> {/* Marcador na localização da loja */}
     
-    {/* Fazer infowindow */}
-    
+    {selected ? (<InfoWindowF
+      position={center}
+      zIndex={1}
+      options={{
+        pixelOffset: {
+          width:0,
+          height:-40,
+        }
+      }}
+    >
+      <div>
+        <h6>Elegance Rommanel</h6>
+        <p>
+          Rod. Amaral Peixoto, 5231 - Centro <br/>
+          Rio das Ostras - RJ <br/>
+          28890-000 <br/>
+          Brasil
+        </p>
+        <Link 
+        href='https://maps.google.com/maps?ll=-22.523965,-41.939494&z=20&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=7967111427536696554'
+        >Visualize no Google Maps</Link>
+      </div>
+    </InfoWindowF>) : null}
     </GoogleMap>
   );
 };
