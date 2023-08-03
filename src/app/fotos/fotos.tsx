780768425301dc417c12550d8cc77f04e34844ba
Lucas Travessa <lucas.cos.travessa@gmail.com>
2023-08-03 18:14:46 -0300

    estilização e fotos

--- src/app/fotos/fotos.tsx
+++ src/app/fotos/fotos.tsx
@@ -1,50 +1,48 @@
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { InstagramEmbed } from 'react-social-media-embed';
 
 export default function Fotos() {
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex:number) => {
+    setIndex(selectedIndex);
+  }
+
   return (
     <div className="bgPrimary">
-      <Container className="my-5 p-5 bgPrimary">
+
+      <Container className="d-none d-lg-block p-5 bgPrimary">
         <Row>
-        <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the cards content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+        <Col className="d-flex justify-content-center pb-3">
+            <InstagramEmbed url="https://www.instagram.com/p/CvSpMufL9Ka/" width={328} height={656} />
           </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the cards content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <Col className="d-flex justify-content-center pb-3">
+            <InstagramEmbed url="https://www.instagram.com/p/CvQXcGsuGDs/" width={328} height={656} />
           </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the cards content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <Col className="d-flex justify-content-center">
+            <InstagramEmbed url="https://www.instagram.com/p/CvNxnRrO_m_/" width={328} height={656} />
           </Col>
         </Row>
+      </Container>
+
+      {/* Mobile carrousel */}
+      <Container className="d-lg-none d-flex justify-content-center">
+        <Carousel slide={false} activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <InstagramEmbed url="https://www.instagram.com/p/CvSpMufL9Ka/" width={328} height={656} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <InstagramEmbed url="https://www.instagram.com/p/CvQXcGsuGDs/" width={328} height={656} /> 
+          </Carousel.Item>
+          <Carousel.Item>
+            <InstagramEmbed url="https://www.instagram.com/p/CvNxnRrO_m_/" width={328} height={656} />
+          </Carousel.Item>
+        </Carousel>
       </Container>
     </div>
   );
